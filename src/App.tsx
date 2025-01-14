ffd130fcfedf235094ab178e8619304241bd57f2
Andrew Lawendy <22712035+AndrewLawendy@users.noreply.github.com>
2025-01-14 18:10:43 +0200

    feat(splash): 🎉 add view (#1)
    * chore(on save): :wrench: enfore style
    
    * feat(favicon): :sparkles: add
    
    * fix(favicon): :bug: fix
    
    * feat(splash): :tada: add
    
    ---------
    
    Co-authored-by: AndrewLawendy <andrew.lawendy@lawendy.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
+import reactLogo from "./assets/react.svg";
+import SplashWrapper from "./components/SplashWrapper";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <SplashWrapper>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </SplashWrapper>
   );
 }
 
